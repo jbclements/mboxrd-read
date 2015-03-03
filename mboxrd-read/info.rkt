b1efd54820de1f159a5b51213ca3cb556ad0da48
John Clements <clements@racket-lang.org>
2015-03-03 12:15:54 -0800

    added scribblings link

--- mboxrd-read/info.rkt
+++ mboxrd-read/info.rkt
@@ -4,3 +4,5 @@
 (define name "Library for Parsing mboxrd files")
 (define blurb `("A library of 2 functions for parsing files in the standard mboxrd format"))
 (define categories `(io))
+
+(define scribblings '(("mboxrd-read.scrbl")))
