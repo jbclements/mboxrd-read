fe1fa607c8efabe267f30a8c9a321e823c775dbd
John Clements <clements@racket-lang.org>
2019-09-18 10:10:53 -0700

    add code to count unread messages in maildirs

--- mboxrd-read/maildir.rkt
+++ mboxrd-read/maildir.rkt
@@ -12,16 +12,24 @@
          racket/stream
          racket/match
          racket/string
-         net/head)
+         net/head
+         (only-in racket/list count))
 
 (provide (contract-out
           [maildir-parse
                    (->* (path-string?)
                         ()
                         (stream/c (list/c (or/c false? bytes?)
-                                          (-> (or/c false? input-port?)))))]))
+                                          (-> (or/c false? input-port?)))))]
+          [maildir-num-unread
+           (-> path-string? exact-nonnegative-integer?)]))
 
 (define (false? v) (eq? v #f))
+
+(define (maildir? path)
+  (and (directory-exists? path)
+       (directory-exists? (build-path path "new"))
+       (directory-exists? (build-path path "cur"))))
 
 ;; given a path to a maildir, return a lazy list of the messages in the 
 ;; file.  Each file is represented as a list containing a byte-string
@@ -30,17 +38,11 @@
 ;; message except that every \n in the original is replaced by \r\n to
 ;; match the RFC 2822 format.
 (define (maildir-parse path)
-  (unless (and (directory-exists? path)
-               (directory-exists? (build-path path "new"))
-               (directory-exists? (build-path path "cur")))
+  (unless (maildir? path)
     (raise-argument-error 'maildir-parse
                           "name of existing directory with new and cur subdirectories"
                           0 path))
-  (define mail-file-list
-    (filter file-exists?
-            (append
-             (directory-list (build-path path "new") #:build? #t)
-             (directory-list (build-path path "cur") #:build? #t))))
+  (define mail-file-list (maildir-files path))
   (let loop ([files-list mail-file-list])
     (cond [(null? files-list)
            empty-stream]
@@ -49,6 +51,13 @@
                        (λ () (message-body-port (car files-list))))
                  (loop (cdr files-list)))])))
 
+;; return a list of the mail files in a maildir (anything in 'new' or
+;; 'cur' that's a file)
+(define (maildir-files path)
+  (filter file-exists?
+          (append
+           (directory-list (build-path path "new") #:build? #t)
+           (directory-list (build-path path "cur") #:build? #t))))
 
 ;; given a path, return the message's headers. If the file is missing,
 ;; return #f instead.
@@ -81,6 +90,33 @@
                     #"\r\n")
    #"\r\n\r\n"))
 
+;; count the unread messages in a mailbox. Not worrying about locks,
+;; in line with maildir philosophy
+(define (maildir-num-unread path)
+  (unless (maildir? path)
+    (raise-argument-error 'maildir-num-unread
+                          "name of existing directory with new and cur subdirectories"
+                          0 path))
+  (define files (maildir-files path))
+  (count unread-message? files))
+
+(define (unread-message? path)
+  (define-values (base name must-be-dir?) (split-path path))
+  (match (path->string name)
+    ;; no colon, must be new:
+    [(regexp #px"^[^:]+$") #t]
+    ;; note that capital flags must come first, by the spec at
+    ;; https://cr.yp.to/proto/maildir.html,
+    [(regexp #px"^[^:]+:2,([A-Z]*)" (list _ capital-flags))
+     (not (member #\S (string->list capital-flags)))]
+    [(regexp #px"^[^:]+:1,")
+     (raise-argument-error 'unread-message?
+                           "message name not using experimental semantics"
+                           0 path)]
+    [other
+     (raise-argument-error 'unread-message
+                           "recognized message name format"
+                           0 path)]))
 
 
 ;; TESTING
@@ -89,6 +125,14 @@
   (require rackunit
            racket/block)
 
+  (check-equal? (unread-message? (build-path "/abc" "def" "ghi:2,abc"))
+                #t)
+  (check-equal? (unread-message? (build-path "/abc" "def" "ghi:2,Sabc"))
+                #f)
+  (check-exn #px"experimental semantics"
+             (λ () (unread-message? (build-path "/abc" "def" "ghi:1,Sabc"))))
+  (check-exn #px"recognized message name format"
+             (λ () (unread-message? (build-path "/abc" "def" "ghi::1,Sabc"))))
 
   (check-exn #px"expected: name of existing directory" 
              (λ () (maildir-parse "/tmp/not-a-maildir-12374279834918/")))
@@ -97,6 +141,9 @@
 
   (check-equal? (message-body-port "/tmp/bogus-file-!!@#$HT#@H")
                 #f)
+
+  (check-exn #px"expected: name of existing directory" 
+             (λ () (maildir-num-unread "/tmp/not-a-maildir-12374279834918/")))
   
 
   (check-exn
--- mboxrd-read/tests.rkt
+++ mboxrd-read/tests.rkt
@@ -1,67 +1,73 @@
 #lang racket
 
-(require "main.rkt"
-         "mboxcl2.rkt"
-         "maildir.rkt"
-         net/head
-         rackunit
-         racket/runtime-path)
+(module+ test
+  (require "main.rkt"
+           "mboxcl2.rkt"
+           "maildir.rkt"
+           net/head
+           rackunit
+           racket/runtime-path)
 
-;; this really just tests that a simple mbox file comes out with the right
-;; number of messages.  Further testing would be nice...
+  ;; this really just tests that a simple mbox file comes out with the right
+  ;; number of messages.  Further testing would be nice...
 
-(define-runtime-path here ".")
-(define example-mbox (build-path here "test-mbox"))
+  (define-runtime-path here ".")
+  (define example-mbox (build-path here "test-mbox"))
 
-(define mail-stream
-  (mboxrd-parse example-mbox))
+  (define mail-stream
+    (mboxrd-parse example-mbox))
 
-(let loop ([n 0] [stream mail-stream])
-  (cond [(< n 5)
-         (define first-mail (stream-first stream))
-         (check-match
-          first-mail
-          (list (? bytes? b1) (? bytes? b2)))
-         (validate-header
-          (cadr
-           (regexp-match #px"\r\n(.*)$" (first first-mail))))
-         (loop (add1 n) (stream-rest stream))]
-        [else
-         (check-true (stream-empty? stream))]))
+  (let loop ([n 0] [stream mail-stream])
+    (cond [(< n 5)
+           (define first-mail (stream-first stream))
+           (check-match
+            first-mail
+            (list (? bytes? b1) (? bytes? b2)))
+           (validate-header
+            (cadr
+             (regexp-match #px"\r\n(.*)$" (first first-mail))))
+           (loop (add1 n) (stream-rest stream))]
+          [else
+           (check-true (stream-empty? stream))]))
 
-(define-values (closer mail-stream2)
-  (mboxcl2-parse example-mbox #:fallback #t))
+  (define-values (closer mail-stream2)
+    (mboxcl2-parse example-mbox #:fallback #t))
 
-(let loop ([n 0] [stream mail-stream2])
-  (cond [(< n 5)
-         (define first-mail (stream-first stream))
-         (check-match
-          first-mail
-          (list (? bytes? b1) body-thunk))
-         (validate-header
-          (cadr
-           (regexp-match #px"\r\n(.*)$" (first first-mail))))
-         (check-true (bytes? ((cadr first-mail))))
-         (loop (add1 n) (stream-rest stream))]
-        [else
-         (check-true (stream-empty? stream))]))
+  (let loop ([n 0] [stream mail-stream2])
+    (cond [(< n 5)
+           (define first-mail (stream-first stream))
+           (check-match
+            first-mail
+            (list (? bytes? b1) body-thunk))
+           (validate-header
+            (cadr
+             (regexp-match #px"\r\n(.*)$" (first first-mail))))
+           (check-true (bytes? ((cadr first-mail))))
+           (loop (add1 n) (stream-rest stream))]
+          [else
+           (check-true (stream-empty? stream))]))
 
-(closer)
+  (closer)
 
-(define test-stream (maildir-parse (build-path here "example-maildir")))
+  (define example-maildir (build-path here "example-maildir"))
 
-(check-equal? (stream-length test-stream) 3)
-(check-not-exn (λ () (validate-header (first (stream-first test-stream)))))
-(check-equal? (extract-field #"Message-ID" (first (stream-ref test-stream 2)))
-              #"<2005123351228.002YI0i8I9@mailer.johnkerry.com>")
-(check-equal? (regexp-match #px"vehemently with"
-                            ((second (stream-ref test-stream 1))))
-              '(#"vehemently with"))
+  (define test-stream (maildir-parse example-maildir))
 
-(check-equal? (stream-length (maildir-parse (build-path here
-                                                        "example-maildir"
-                                                        ".sub-maildir")))
-              2)
+  (check-equal? (stream-length test-stream) 3)
+  (check-not-exn (λ () (validate-header (first (stream-first test-stream)))))
+  (check-equal? (extract-field #"Message-ID" (first (stream-ref test-stream 2)))
+                #"<2005123351228.002YI0i8I9@mailer.johnkerry.com>")
+  (check-equal? (regexp-match #px"vehemently with"
+                              ((second (stream-ref test-stream 1))))
+                '(#"vehemently with"))
+
+  (check-equal? (stream-length (maildir-parse (build-path example-maildir
+                                                          ".sub-maildir")))
+                2)
+
+  (check-equal? (maildir-num-unread example-maildir) 2))
+
+
 
 
 
